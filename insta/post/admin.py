37132a1e59f76f553b86174e0315ac917355bac9
ShohruhAbdurahmonov <shohruhabdurahmonov800@gmail.com>
2024-07-02 07:39:44 +0500

    first commit

---
+++ insta/post/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from .models import Post, PostLike, PostComment, CommentLike
+
+
+admin.site.register(Post)
+admin.site.register(PostComment)
+admin.site.register(PostLike)
+admin.site.register(CommentLike)
+
